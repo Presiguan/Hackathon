31dde23788032afd1b9fb570cc96faa3c904920d
presiguan <abbey14goal@yahoo.com>
2023-05-03 00:57:41 +0100

    complete update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Card from "./Components/carCards";
 import HeroCard from "./Components/heroCard";
 import HeroCard2 from "./Components/heroCard2";
 import Hero from "./Components/hero";
+import Download from "./Components/download";
+
 function App() {
   return (
     <>
@@ -12,6 +14,7 @@ function App() {
       <Banner />
       <HeroCard />
       <HeroCard2 />
+      <Download />
       <Hero />
       {/* <Card /> */}
     </>
